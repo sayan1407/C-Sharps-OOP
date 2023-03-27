39321dec92806a0ede30bf3224dc89f3ca74b53b
sayan saha <sayan199783@gmail.com>
2023-03-27 12:19:03 +0530

    OOP-Encapculation2

---
+++ C-Sharps-OOP-Practice/Post.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharps_OOP_Practice
+{
+    class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        private int _totalvote;
+        
+
+        public Post(int id,string title,string description)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Description = description;
+            CreatedDate = DateTime.Now;
+            _totalvote = 0;
+        }
+        public int TotalVote
+        {
+            get
+            {
+                if (_totalvote <= 0)
+                    return 0;
+                else
+                    return _totalvote;
+            }
+            private set
+            {
+                _totalvote = value;
+            }
+        }
+        public void UpVote()
+        {
+            _totalvote++;
+        }
+        public void DownVote()
+        {
+
+            _totalvote--;
+        }
+
+    }
+}
--- C-Sharps-OOP-Practice/Program.cs
+++ C-Sharps-OOP-Practice/Program.cs
@@ -10,10 +10,25 @@ namespace C_Sharps_OOP_Practice
         {
             try
             {
-                StopWatch stopWatch = new StopWatch();
-                stopWatch.Start();
-                stopWatch.Stop();
-                Console.WriteLine(stopWatch.TimeDifference.TotalMilliseconds);
+                //StopWatch stopWatch = new StopWatch();
+                //stopWatch.Start();
+                //stopWatch.Stop();
+                //Console.WriteLine(stopWatch.TimeDifference.TotalMilliseconds);
+                var post = new Post(1, "SQL Create Table", "SQL query to create a table");
+                post.UpVote();
+                post.DownVote();
+                post.DownVote();
+                post.DownVote();
+                post.UpVote();
+                post.UpVote();
+                post.UpVote();
+                Console.WriteLine("Title : {0},Description : {1},  TotalVote : {2}",post.Title,post.Description,post.TotalVote);
+                var post1 = new Post(2, "SQL Insert Table", "SQL query to insert to a table");
+                post1.UpVote();
+                post1.UpVote();
+                post1.UpVote();
+                post1.DownVote();
+                Console.WriteLine("Title : {0},Description : {1},  TotalVote : {2}", post1.Title, post1.Description, post1.TotalVote);
             }
             catch(Exception ex)
             {
