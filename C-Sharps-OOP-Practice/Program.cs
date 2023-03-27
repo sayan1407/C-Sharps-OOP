5032c0d5b3a41a8bc16da8a130949938a2204319
sayan saha <sayan199783@gmail.com>
2023-03-27 11:40:13 +0530

    OOP-Encapculation1

---
+++ C-Sharps-OOP-Practice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+
+namespace C_Sharps_OOP_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StopWatch stopWatch = new StopWatch();
+                stopWatch.Start();
+                stopWatch.Stop();
+                Console.WriteLine(stopWatch.TimeDifference.TotalMilliseconds);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ C-Sharps-OOP-Practice/StopWatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharps_OOP_Practice
+{
+    public class StopWatch
+    {
+        public DateTime StartTime { get; set; }
+        public string PreviousAction { get; private set; }
+        public TimeSpan TimeDifference { get; private set; }
+
+        public StopWatch()
+        {
+            PreviousAction = "stop";
+        }
+        public void Start()
+        {
+            if (PreviousAction == "start")
+                throw new InvalidOperationException("Please first stop the watch");
+            StartTime = DateTime.Now;
+            PreviousAction = "start";
+        }
+        public void Stop()
+        {
+            if (PreviousAction == "stop")
+                throw new InvalidOperationException("Please first start the watch");
+            TimeDifference = DateTime.Now - StartTime;
+            PreviousAction = "stop";
+        }
+    }
+}
